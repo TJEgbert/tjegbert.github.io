38de9494056e41828ce5a51adef92ee7dc465e6a
TJEgbert <trevoregbert@mail.weber.edu>
2024-08-10 13:57:24 -0600

    assignment 2 added

---
+++ Connect Four/src/index.js
@@ -0,0 +1,13 @@
+//Starter page for react pages
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
